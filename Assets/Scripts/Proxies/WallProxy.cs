df39d300cf95d1d4e2d55d52383f2c32ade36881
nhlee <noirhero83@gmail.com>
2019-10-19 02:59:16 +0900

    Wall 시스템 추가.

---
+++ Assets/Scripts/Proxies/WallProxy.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2018-2019 TAP, Inc. All Rights Reserved.
+
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+using Components;
+
+namespace Proxies {
+    [DisallowMultipleComponent]
+    [RequiresEntityConversion]
+    public class WallProxy : MonoBehaviour, IConvertGameObjectToEntity {
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+            var boxCollision = GetComponent<BoxCollider2D>();
+            if (null == boxCollision) {
+                Debug.LogError("Add component 'BoxCollider2D', now!!!!!");
+                dstManager.DestroyEntity(entity);
+                return;
+            }
+
+            dstManager.RemoveComponent<LocalToWorld>(entity);
+            dstManager.RemoveComponent<Rotation>(entity);
+            dstManager.RemoveComponent<Translation>(entity);
+
+            var pos = transform.position;
+            dstManager.AddComponentData(entity, new WallComponent() {
+                pos = new float2(pos.x, pos.y),
+                size = boxCollision.size,
+                offset = boxCollision.offset
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/WallSystem.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2018-2019 TAP, Inc. All Rights Reserved.
+
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+using Components;
+
+namespace Systems {
+    [UpdateAfter(typeof(InputMovementSystem))]
+    public class WallSystem : ComponentSystem {
+        protected override void OnUpdate() {
+            Entity playerAvatarEntity = Entity.Null;
+            Entities.ForEach((Entity entity, ref PlayerAvatarComponent playerAvatarComp) => {
+                playerAvatarEntity = entity;
+            });
+            if (Entity.Null == playerAvatarEntity) {
+                return;
+            }
+
+            Entities.ForEach((ref WallComponent wallComp) => {
+                var playerPos = EntityManager.GetComponentData<Translation>(playerAvatarEntity).Value;
+                var boxPosX = wallComp.pos.x + wallComp.offset.x;
+                var posAtLength = math.abs(boxPosX - playerPos.x);
+                var halfBoxSizeX = wallComp.size.x * 0.5f;
+                if (halfBoxSizeX <= posAtLength) {
+                    return;
+                }
+
+                float at = (wallComp.pos.x < playerPos.x) ? 1.0f : -1.0f;
+                playerPos.x = boxPosX + at * halfBoxSizeX;
+
+                EntityManager.SetComponentData(playerAvatarEntity, new Translation() {
+                    Value = playerPos
+                });
+            });
+        }
+    }
+}
